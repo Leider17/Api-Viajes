4b140313ba2ebea3241c3eebd7e8d35d7e81f954
Leider17 <leidersolano133@gmail.com>
2025-02-27 22:59:49 -0500

    modification in route names

--- routes/api.php
+++ routes/api.php
@@ -8,20 +8,17 @@ use App\Http\Controllers\DestinationController;
 use App\Http\Controllers\HotelController;
 use App\Http\Controllers\UserController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
-Route::post('/login',[UserController::class,'login'])->name('users.login');
+
+Route::post('/login',[UserController::class,'loginUser'])->name('login');
 Route::post('/register', [UserController::class, 'createUser'])->name('users.register');
+    
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/destinations', [DestinationController::class, 'store'])->name('destinations.store');
+    Route::delete('/destinations/{destination}', [DestinationController::class, 'destroy'])->name('destinations.destroy');
+    Route::put('/destinations/{destination}', [DestinationController::class, 'update'])->name('destinations.update');
 
 
-Route::middleware('auth:sactum')->group(function () {
-    Route::post('/Destinations', [DestinationController::class, 'store'])->name('destinations.store');
-    Route::put('/Destinations/{destination}', [DestinationController::class, 'update'])->name('destinations.update');
-    Route::delete('/Destinations/{destination}', [DestinationController::class, 'destroy'])->name('destinations.destroy');
-
-    Route::post('/createUser', [UserController::class, 'createUser'])->name('users.createUser');
 
     Route::post('/hotels', [HotelController::class, 'store'])->name('hotels.store');
     Route::put('/hotels/{hotel}', [HotelController::class, 'update'])->name('hotels.update');
@@ -36,14 +33,14 @@ Route::middleware('auth:sactum')->group(function () {
     Route::delete('/activities/{activity}', [ActivityController::class, 'destroy'])->name('activities.destroy');
 });
 
-Route::get('/Destinations', [DestinationController::class, 'index'])->name('destinations.index');
-Route::get('/Destinations/{destination}', [DestinationController::class, 'show'])->name('destinations.show');
-Route::get('/Destinations/Comments', [DestinationController::class, 'DestinationsComments'])->name('destinationscomments.index');
-Route::get('/Destinations/{destination}/Comments', [DestinationController::class, 'DestinationComments'])->name('destinationscomments.show');
-Route::get('/Destinations/Hotels', [DestinationController::class, 'DestinationsHotels'])->name('destinationshotels.index');
-Route::get('/Destinations/{destination}/Hotels', [DestinationController::class, 'DestinationHotels'])->name('destinationshotels.show');
-Route::get('/Destinations/Activities', [DestinationController::class, 'DestinationsActivities'])->name('destinationsactivities.index');
-Route::get('/Destinations/{destination}/Activities', [DestinationController::class, 'DestinationActivities'])->name('destinationsactivities.show');
+Route::get('/destinations', [DestinationController::class, 'index'])->name('destinations.index');
+Route::get('/destinations/{destination}', [DestinationController::class, 'show'])->name('destinations.show');
+Route::get('/destinations/comments', [DestinationController::class, 'DestinationsComments'])->name('destinationscomments.index');
+Route::get('/destinations/{destination}/comments', [DestinationController::class, 'DestinationComments'])->name('destinationscomments.show');
+Route::get('/destinations/Hotels', [DestinationController::class, 'DestinationsHotels'])->name('destinationshotels.index');
+Route::get('/destinations/{destination}/hotels', [DestinationController::class, 'DestinationHotels'])->name('destinationshotels.show');
+Route::get('/destinations/Activities', [DestinationController::class, 'DestinationsActivities'])->name('destinationsactivities.index');
+Route::get('/destinations/{destination}/activities', [DestinationController::class, 'DestinationActivities'])->name('destinationsactivities.show');
 
 
 Route::get('/users/{user}/reservations', [UserController::class, 'userReservations'])->name('users.reservations');
